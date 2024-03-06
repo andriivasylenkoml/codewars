9b37d36ead8cb1f732ea9c3c48229d5f27154a92
AndriiVasylenko-bit <livekidos95@gmail.com>
2024-03-06 22:21:25 +0200

    modified:   8kyu/total_amount_of_points.py

--- 8kyu/total_amount_of_points.py
+++ 8kyu/total_amount_of_points.py
@@ -1,8 +1,7 @@
 def points(games):
-    points = None
+    points = 0
     for m in games:
         m = m.split(':')
         if m[0] > m[1]: points += 3
-        if m[0] < m[1]: points += 0
         if m[0] == m[1]: points += 1
     return points
