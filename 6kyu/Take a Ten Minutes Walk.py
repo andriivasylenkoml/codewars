f82ba42ded1f22b3121f03358618eeb2efe8e880
AndriiVasylenko-bit <livekidos95@gmail.com>
2024-04-05 23:55:24 +0300

    new file:   6kyu/Take a Ten Minutes Walk.py

---
+++ 6kyu/Take a Ten Minutes Walk.py
@@ -0,0 +1,8 @@
+def is_valid_walk(walk):
+    if len(walk) != 10:
+        return False
+    if walk.count('n') != walk.count('s'):
+        return False
+    if walk.count('w') != walk.count('e'):
+        return False
+    return True
